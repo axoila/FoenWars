837f87c0d56d6ecbd0ea2e790272f39bcd910d0b
Ronja <axoila@gmx.de>
2016-02-04 22:51:29 +0100

    enemies exist and can move :space_invader:

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function init() {
 }
 
 function Game(){
-    this.mode = 0; //0 is story | 1 in running | 2 is paused
+    this.mode = 0; // 0 is intro // 1 is menu // 2 is play // 3 is paused // 4 is init play
     this.enemies = [];
     this.init = function() {
         if(this.setupCanvases()){
@@ -21,6 +21,11 @@ function Game(){
             case 0:
                 this.intro();
                 break;
+			case 2:
+				this.play(delta);
+				break;
+			case 4:
+				this.initPlay();
         }
     }
 
@@ -38,13 +43,45 @@ function Game(){
                 this.background.draw(imageRepository.story3);
                 break;
             default:
-                this.mode = 1;
+                this.mode = 4;
         }
         if(KEY_STATUS.lmb){
             this.page++;
             KEY_STATUS.lmb = false;
         }
     }
+
+	this.initPlay = function(){
+		this.mode = 2;
+		this.enemySpawnFrequency = 0.2;
+		this.enemySpawnCounter = 0;
+		this.enemies = [];
+		Enemy.prototype.context.clearRect(0, 0, Enemy.prototype.contextWidth, Enemy.prototype.contextHeight);
+		this.background.draw(imageRepository.background);
+	}
+
+	this.play = function(delta){
+		this.enemySpawnCounter+=delta;
+		if(this.enemySpawnCounter>=1/this.enemySpawnFrequency){
+			this.enemySpawnCounter =0;
+			var newEnemy = new Enemy();
+			this.enemies.push(newEnemy);
+			console.log("spawned enemy");
+		}
+
+		//first calculate the new enemy position without redrawing anything
+		this.enemies.forEach(function(enemy){
+			enemy.update(delta);
+		});
+		/*first clear all enemies, then draw them again, so they are off
+		the screen for as short of a time span as possible*/
+		this.enemies.forEach(function(enemy){
+			enemy.clear();
+		});
+		this.enemies.forEach(function(enemy){
+			enemy.draw();
+		});
+	}
 
     this.setupCanvases = function(){
         this.bgCanvas = document.getElementById('background');
@@ -58,9 +95,8 @@ function Game(){
 			this.playerContext = this.playerCanvas.getContext('2d');
 			this.hudContext = this.hudCanvas.getContext('2d');
 
-            Background.prototype.context = this.bgContext;
-			Background.prototype.canvasWidth = this.bgCanvas.width;
-			Background.prototype.canvasHeight = this.bgCanvas.height;
+            Background.prototype.setContext(this.bgContext);
+			Enemy.prototype.setContext(this.enemyContext);
 
             return true;
         } else {
@@ -73,13 +109,34 @@ function Game(){
 	}
 }
 
+function Enemy() {
+	this.x = 1280;
+	this.y = 400;
+	this.width = 128;
+	this.height = 256;
+	this.moveSpeed = 30; //speed at whick the enemy walks towards you (in px/sec)
+	this.animCycle = 0; //used for decaying and walkcycle
+	this.alive = true; //is the enemy alive?
+    this.image = imageRepository.no;
+	this.clear = function() {
+		this.context.clearRect(this.x, this.y, this.width+1, this.height);
+	};
+	this.draw = function() {
+        this.context.drawImage(this.image, this.x, this.y, this.width, this.height );
+	};
+	this.update = function(delta) {
+		this.x -= Math.min(this.moveSpeed * delta, 1);
+		//console.log(this.x+" | "+this.y);
+	};
+}
+Enemy.prototype = new Drawable();
 
 function Background(){
     this.image = imageRepository.background;
     this.context.drawImage(this.image, 0, 0);
 	this.draw = function(newImage){
         if(this.image != newImage){
-            console.log(newImage);
+            //console.log(newImage);
             this.image = newImage;
 			this.context.drawImage(this.image, 0, 0);
 		}
@@ -95,7 +152,7 @@ function animate(){
 	//console.log(delta);
 	lastUpdate = now;
 
-    game.tick();
+    game.tick(delta);
 
 	requestAnimFrame(animate);
 }
@@ -119,6 +176,11 @@ function Drawable(){
 		this.width = width;
 		this.height = height;
 	}
+	this.setContext = function(context){
+		this.context = context;
+		this.canvasWidth = context.width;
+		this.canvasHeight = context.height;
+	}
 	this.speed = 0;
 	this.canvasWidth = 0;
 	this.canvasHeight = 0;
@@ -128,6 +190,7 @@ function Drawable(){
 
 var imageRepository = new function() {
     this.background = new Image();
+	this.no = new Image();
     this.story1 = new Image();
     this.story2 = new Image();
     this.story3 = new Image();
@@ -135,24 +198,26 @@ var imageRepository = new function() {
 		window.init();
 	}
     this.background.src = "imgs/background_programmer-art.png";
+	this.no.src = "imgs/NO.png";
     this.story1.src = "imgs/backgroundstory1.png";
     this.story2.src = "imgs/backgroundstory2.png";
     this.story3.src = "imgs/backgroundstory3.png";
 }
 
+
 //ALL THINGS BELOW ARE MAGIC
 
 // The keycodes that will be mapped when a user presses a button.
 KEY_CODES = {
-  32: 'space',
-  37: 'left',
+  32: 'lmb',
   38: 'up',
-  39: 'right',
+  37: 'left',
   40: 'down',
-  77: 'm',
-  78: 'n',
-  89: 'y',
-  82: 'r',
+  39: 'right',
+  87: 'up',
+  65: 'left',
+  83: 'down',
+  68: 'right',
   0: 'lmb',
   2: 'rmb',
 }
